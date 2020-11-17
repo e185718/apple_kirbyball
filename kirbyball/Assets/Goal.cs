2272b21b32c41f0d2e22448cd86b53bd7bb0bc79
e185721 <e185721@ie.u-ryukyu.ac.jp>
2020-11-17 16:40:10 +0900

    ゴール判定の追加、遷移まで

---
+++ kirbyball/Assets/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class Goal : MonoBehaviour {
+ 
+    //プレイヤーが当たり判定に入った時の処理
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Scenes/stage1_clear");
+        }
+    }
+ 
+}
+
